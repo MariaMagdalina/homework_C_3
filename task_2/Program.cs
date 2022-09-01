616a34e637e83d97a4d1932148e38cb9ae3d24f3
MariaMagdalina <metodist_rmmc@mail.ru>
2022-09-01 10:40:55 +0300

    задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа рассчитывает расстояние между двумя точками в пространстве
+// координаты точки А
+Console.Write("Введите координаты точки А. xa = ");
+int xa = Convert. ToInt32(Console.ReadLine());
+Console.Write("ya = ");
+int ya = Convert. ToInt32(Console.ReadLine());
+Console.Write("za = ");
+int za = Convert. ToInt32(Console.ReadLine());
+
+// координаты точки В
+Console.Write("Введите координаты точки B. xb = ");
+int xb = Convert. ToInt32(Console.ReadLine());
+Console.Write("yb = ");
+int yb = Convert. ToInt32(Console.ReadLine());
+Console.Write("zb = ");
+int zb = Convert. ToInt32(Console.ReadLine());
+
+// находим расстояние между точками по формуле
+double s; // расстояние между точками
+s = Math.Sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb) + (za-zb)*(za-zb));
+Console.WriteLine("расстояние между точкой А и точкой В равно " + s);
