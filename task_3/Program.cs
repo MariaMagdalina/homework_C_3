d6293b81b2d49ea751763c7a73d51e7e748a7270
MariaMagdalina <metodist_rmmc@mail.ru>
2022-09-01 10:43:31 +0300

    задание 3

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа формирует массив из кубов чисел от 1 до числа, полученного пользователем на входе
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// обьявляем новый массив и заполняем его в цикле кубами чисел
+int [] array_cubed = new int [number];
+for (int i=0; i<number; i++)
+{
+   array_cubed[i] = (i+1)*(i+1)*(i+1);
+   Console.WriteLine(array_cubed[i]);
+} 
